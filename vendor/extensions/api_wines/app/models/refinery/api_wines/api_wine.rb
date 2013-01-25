734984cf9e35196a7cff93dc5e4ee4befabf73b5
shanhua <shanhua.chen@sidways.com>
2013-01-25 16:03:35 +0800

    api_tasting_flight 隐藏图片； api_wine 允许编辑图片。

--- vendor/extensions/api_wines/app/models/refinery/api_wines/api_wine.rb
+++ vendor/extensions/api_wines/app/models/refinery/api_wines/api_wine.rb
@@ -8,7 +8,7 @@ module Refinery
       
       belongs_to :image, :class_name => '::Refinery::Image'
 
-      attr_accessible :type, :vintage, :name_en, :name_zh, :region, :wine_style, :capacity, :alcoholicity, :variety, :rating, :rating_rp, :rating_ws, :rating_jr, :price, :info, :cellar_num, :cellar_location, :slug, :image_id, :position, :wine_id
+      attr_accessible :type, :vintage, :name_en, :name_zh, :region, :wine_style, :capacity, :alcoholicity, :variety, :rating, :rating_rp, :rating_ws, :rating_jr, :price, :info, :cellar_num, :cellar_location, :slug, :image_id, :position, :wine_id, :sku
 
       acts_as_indexed :fields => [:type, :vintage, :name_en, :name_zh, :region, :wine_style, :capacity, :alcoholicity, :variety, :rating, :rating_rp, :rating_ws, :rating_jr, :info, :cellar_num, :cellar_location, :slug]
 
